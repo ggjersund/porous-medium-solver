0c1a96e9faf7508e4c85fb43a4e10c4d822a7e4b
Gjert Ingar Gjersund <gjertig@stud.ntnu.no>
2019-03-28 20:31:51 +0100

    Update src

---
+++ src/classes.py
@@ -0,0 +1,110 @@
+import numpy as np
+from scipy import signal
+from scipy.optimize import fsolve
+
+
+class PorousMediumEquation(object):
+
+    def __init__(self, m=1, f=0, g1=0, g2=0, M=10, N=10, T_low=0, T_high=2, X_low=-3, X_high=3):
+
+        # Set m
+        self.m = m
+
+        # Initial condition
+        self.f = f
+
+        # Boundary conditions
+        self.g1 = g1
+        self.g2 = g2
+
+        # Set axis limits
+        self.X_low = X_low
+        self.X_high = X_high
+        self.T_low = T_low
+        self.T_high = T_high
+
+        # Set gridsize
+        self.M = M
+        self.N = N
+
+        # Create grid
+        self.x = np.linspace(self.X_low, self.X_high, self.M+1)
+        self.t = np.linspace(self.T_low, self.T_high, self.N+1)
+
+        # Create solution grid
+        self.U = np.zeros((self.M+1, self.N+1))
+        self.impulses = np.zeros(self.M+1)
+
+        # Set stepsize
+        self.h = (self.X_high - self.X_low) / self.M
+        self.k = (self.T_high - self.T_low) / self.N
+        self.r = (1/(self.m+1)) * (self.k / self.h**2)
+
+    def __str__(self):
+        return "Porous Medium Equation Object"
+
+    @staticmethod
+    def tridiag(a, b, c, N):
+        # Create tri-diagonal matrix
+        e = np.ones(N)
+        A = a*np.diag(e[1:], -1) + b*np.diag(e) + c*np.diag(e[1:], 1)
+        return A
+
+    def add_impulse(self, index, ratio=1/4):
+        # Adds discrete delta function at given index
+        # Note that Forward-Euler can have convergence problems for impulses
+        if index < (self.M + 1):
+            self.impulses[:] += signal.unit_impulse(self.M+1, index) * ratio
+
+    def forward_euler(self):
+
+        # Reset solution grid
+        self.U = np.zeros((self.M+1, self.N+1))
+
+        # Check CFL condition (more strict that usual 1/2)
+        if ((self.k * np.amax(self.f(self.x))**(self.m) / self.h**2) >= (1/2)):
+            raise ValueError("CFL condition not satisfied for Forward-Euler method.")
+
+        # Set initial conditions
+        self.U[:, 0] += self.impulses[:]
+        self.U[:, 0] += self.f(self.x)
+
+        # Generate A matrix and b vector
+        A = self.tridiag(1, -2, 1, self.M-1)
+        b = np.zeros(self.M-1)
+
+        for n in range(self.N):
+            b[0] = self.g1(self.t[n])
+            b[-1] = self.g2(self.t[n])
+            self.U[1:-1, n+1] = self.U[1:-1, n] + (self.r * np.dot(A, self.U[1:-1, n]**(self.m+1))) + (self.r * b**(self.m+1))
+            self.U[0, n+1] = b[0]
+            self.U[-1, n+1] = b[-1]
+
+        return self.x, self.t, self.U, self.h, self.k
+
+    def backward_euler(self):
+
+        # Reset solution grid
+        self.U = np.zeros((self.M+1, self.N+1))
+
+        # Set initial conditions
+        self.U[:, 0] += self.impulses[:]
+        self.U[:, 0] += self.f(self.x)
+
+        # Generate A matrix and b vector
+        A = self.tridiag(1, -2, 1, self.M-1)
+        b = np.zeros(self.M-1)
+
+        for n in range(self.N):
+            b[0] = self.g1(self.t[n])
+            b[-1] = self.g2(self.t[n])
+
+            def F(u):
+                g = self.m + 1
+                return u - (self.r * np.dot(A, u ** g)) - self.U[1:-1, n] - (self.r * b ** g)
+
+            self.U[1:-1, n+1] = fsolve(F, self.U[1:-1, n])
+            self.U[0, n+1] = b[0]
+            self.U[-1, n+1] = b[-1]
+
+        return self.x, self.t, self.U, self.h, self.k
--- src/main.py
+++ src/main.py
@@ -1,5 +1,63 @@
+import matplotlib
+# Required for mac
+matplotlib.use('TkAgg')
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+newparams = {'figure.figsize': (8.0, 4.0), 'axes.grid': True,
+             'lines.markersize': 8, 'lines.linewidth': 2,
+             'font.size': 12}
 
 
-if __name__="__main__":
-    pass
+from functions import forward_euler, backward_euler
+from classes import PorousMediumEquation
+
+if __name__ == "__main__":
+
+    def Barenblatt(x, t, m, n):
+        alpha = n / ((n * (m - 1)) + 2)
+        beta = alpha / n
+        k = beta * (m - 1) / (2 * m)
+        s = 0.5 - (k * (np.abs(x)**2)) / (t**(2 * beta))
+        return (1 / (t**alpha)) * ((np.maximum(s, 0))**(1 / (m - 1)))
+
+    def initial(x):
+        return Barenblatt(x, t=0.01, m=1.2, n=1)
+
+    def boundary1(t):
+        return (t-t) + 0
+
+    def boundary2(t):
+        return (t-t) + 0
+
+    porous_forward = PorousMediumEquation(m=1.2, f=initial, g1=boundary1, g2=boundary2, M=90, N=200, T_low=0.01, T_high=2)
+    porous_backward = PorousMediumEquation(m=1.2, f=initial, g1=boundary1, g2=boundary2, M=200, N=200, T_low=0.01, T_high=2)
+    # porous_backward.add_impulse(index=54)
+
+    x1, t1, U1, h1, k1 = porous_forward.forward_euler()
+    x2, t2, U2, h2, k2 = porous_backward.backward_euler()
+
+    fig = plt.figure()
+    plt.clf()
+    ax = fig.gca(projection='3d')
+    X, T = np.meshgrid(t1, x1)
+    ax.plot_wireframe(T, X, U1)
+    ax.plot_surface(T, X, U1, cmap=cm.coolwarm)
+    ax.view_init(azim=75)
+    plt.xlabel('x')
+    plt.ylabel('t')
+    plt.title('Forward-Euler')
+    plt.show()
+
+    fig = plt.figure()
+    plt.clf()
+    ax = fig.gca(projection='3d')
+    X, T = np.meshgrid(t2, x2)
+    ax.plot_wireframe(T, X, U2)
+    ax.plot_surface(T, X, U2, cmap=cm.coolwarm)
+    ax.view_init(azim=75)
+    plt.xlabel('x')
+    plt.ylabel('t')
+    plt.title('Backward-Euler')
+    plt.show()
