64868ee74bb9fb2e4e7c8c183c9d7e0ca4376315
Gjert Ingar Gjersund <gjertig@stud.ntnu.no>
2019-03-28 20:36:47 +0100

    Update functions and main

---
+++ src/functions.py
@@ -0,0 +1,24 @@
+import matplotlib
+# Required for mac
+matplotlib.use('TkAgg')
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+newparams = {'figure.figsize': (8.0, 4.0), 'axes.grid': True,
+             'lines.markersize': 8, 'lines.linewidth': 2,
+             'font.size': 12}
+
+
+def plot_solution(x, t, U, txt='Solution', azim=75):
+    fig = plt.figure()
+    plt.clf()
+    ax = fig.gca(projection='3d')
+    X, T = np.meshgrid(t, x)
+    ax.plot_wireframe(T, X, U)
+    ax.plot_surface(T, X, U, cmap=cm.coolwarm)
+    ax.view_init(azim=azim)
+    plt.xlabel('t')
+    plt.ylabel('x')
+    plt.title(txt)
+    plt.show()
--- src/main.py
+++ src/main.py
@@ -1,17 +1,8 @@
-import matplotlib
-# Required for mac
-matplotlib.use('TkAgg')
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-newparams = {'figure.figsize': (8.0, 4.0), 'axes.grid': True,
-             'lines.markersize': 8, 'lines.linewidth': 2,
-             'font.size': 12}
 
-
-from functions import forward_euler, backward_euler
+from functions import plot_solution
 from classes import PorousMediumEquation
+
 
 if __name__ == "__main__":
 
@@ -38,26 +29,5 @@ if __name__ == "__main__":
     x1, t1, U1, h1, k1 = porous_forward.forward_euler()
     x2, t2, U2, h2, k2 = porous_backward.backward_euler()
 
-    fig = plt.figure()
-    plt.clf()
-    ax = fig.gca(projection='3d')
-    X, T = np.meshgrid(t1, x1)
-    ax.plot_wireframe(T, X, U1)
-    ax.plot_surface(T, X, U1, cmap=cm.coolwarm)
-    ax.view_init(azim=75)
-    plt.xlabel('x')
-    plt.ylabel('t')
-    plt.title('Forward-Euler')
-    plt.show()
-
-    fig = plt.figure()
-    plt.clf()
-    ax = fig.gca(projection='3d')
-    X, T = np.meshgrid(t2, x2)
-    ax.plot_wireframe(T, X, U2)
-    ax.plot_surface(T, X, U2, cmap=cm.coolwarm)
-    ax.view_init(azim=75)
-    plt.xlabel('x')
-    plt.ylabel('t')
-    plt.title('Backward-Euler')
-    plt.show()
+    plot_solution(x1, t1, U1, txt='Solution', azim=75)
+    plot_solution(x2, t2, U2, txt='Solution', azim=75)
